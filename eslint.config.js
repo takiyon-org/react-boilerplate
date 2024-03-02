09ca1fb696927389d80d7f38d5f8b9c7c820e538
Jake Zatecky <jake.zatecky@gmail.com>
2024-03-02 14:42:14 -0500

    Upgrade shared ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,52 +1,21 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import takiyonConfig from 'eslint-config-takiyon-react';
 import globals from 'globals';
 
 import webpackConfig from './webpack.config.js';
 
-const compat = new FlatCompat();
-
 export default [
-    ...compat.extends('eslint-config-takiyon-react'),
+    ...takiyonConfig,
     {
         files: [
-            'src/js/**/*.{js,jsx}',
-            'test/**/*.{js,jsx}',
-            '*.{js,jsx}',
+            '**/*.{js,jsx}',
         ],
-        languageOptions: {
-            // https://github.com/import-js/eslint-plugin-import/issues/2556
-            parserOptions: {
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-            },
-        },
         ignores: ['./node_modules/**/*'],
-        rules: {
-            'import/extensions': ['error', 'ignorePackages', {
-                js: 'always',
-                jsx: 'always',
-                mjs: 'always',
-                cjs: 'never',
-            }],
-            'import/no-extraneous-dependencies': ['error', {
-                devDependencies: [
-                    'test/**',
-                    '*.{js,cjs,mjs}',
-                ],
-                optionalDependencies: false,
-            }],
-        },
         settings: {
             // Account for webpack.resolve.module imports
             'import/resolver': {
                 webpack: {
                     config: webpackConfig,
                 },
-            },
-
-            // https://github.com/import-js/eslint-plugin-import/issues/2556
-            'import/parsers': {
-                espree: ['.js', '.cjs', '.mjs', '.jsx'],
             },
         },
     },
